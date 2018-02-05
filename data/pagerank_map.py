37104043f5e33b5093a0a9732f0450fe4e36fb2f
Westwood <annechen112995@gmail.com>
2018-02-04 21:38:35 -0800

    Updating pagerank_map

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -6,7 +6,28 @@ import sys
 # This program simply represents the identity function.
 #
 
-
+'''
+Example Input: NodeId:0	1.0,0.0,1,2
+Format: NodeId:<IDNumber>\t<currPageRank>,<prevPageRank>,<outlinks>\n
+'''
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+    tab = line.strip().split('\t')
+    content = tab[1].split(',')
+    nodeId = tab[0][7:] # get nodeID number
+    currPageRank = float(content[0])
+
+    # check for outlinks
+    if len(content) > 2:
+        outlinks = content[2:]
+        outlinkValue = str(currPageRank / len(outlinks))
+
+        # print outlinks and value of outlinks
+        for i in outlinks:
+            sys.stdout.write('%s\t%s\n' % (i, outlinkValue))
+        sys.stdout.write('%s\t0\n' % nodeId)
+
+    else:
+        sys.stdout.write('%s\t%s\n' % (nodeId, currPageRank))
+
+    sys.stdout.write('%s\tp,%s\n' % (nodeId, tab[1]))
