ea83ffef56bd0b508b5af8f563c4f54b21cac3e3
Westwood <annechen112995@gmail.com>
2018-02-04 21:47:21 -0800

    Parsing data from input files

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -17,17 +17,4 @@ for line in sys.stdin:
     nodeId = tab[0][7:] # get nodeID number
     currPageRank = float(content[0])
 
-    # check for outlinks
-    if len(content) > 2:
-        outlinks = content[2:]
-        outlinkValue = str(currPageRank / len(outlinks))
-
-        # print outlinks and value of outlinks
-        for i in outlinks:
-            sys.stdout.write('%s\t%s\n' % (i, outlinkValue))
-        sys.stdout.write('%s\t0\n' % nodeId)
-
-    else:
-        sys.stdout.write('%s\t%s\n' % (nodeId, currPageRank))
-
-    sys.stdout.write('%s\tp,%s\n' % (nodeId, tab[1]))
+    sys.stdout.write('%s\t%s\n' % (nodeId, tab[1]))
