39b22dbbf6aa490a211dd07b450e42d8650f015b
Michelle Xiao <yuxiexiao@gmail.com>
2018-02-05 17:59:08 -0800

    Adding changes

--- data/pagerank_reduce.py
+++ data/pagerank_reduce.py
@@ -2,26 +2,56 @@
 
 import sys
 
-#
-# This program simply represents the identity function.
-#
+''' 
+This reducer computes one step of the pageRank algorithm,
+and updates the pageRank of all nodes once. 
+It does so by summing all pageRank contributions that come in
+from the mapper program. 
 
 '''
-Input(s):
-outlink, pr of outlink OR
-nodeId, content(currPageRank, prevPageRank, outlinks)
-'''
+
+# Input:
+# Each input line is formatted as either:
+# 1. NodeId: n_id\tr_contr, p_rank, adj_list
+# or 
+# 2. NodeId: n_id\tr_contr
+#
+# Where:
+#           n_id is the Node id
+#           \t is a tab character
+#           r_contr is the portion of the node's pageRank
+#           p_rank is the previous pagerank of the node
+#           adj_list is the node's neighbors, comma delineated
+#
+#
+# Output:
+# Each output line is formatted as NodeId: n_id\tc_rank, p_rank, adj_list
+# Where:
+#           c_rank is the current pagerank of the node
+#    
+
 
 for line in sys.stdin:
+    # split the line to get our key and values
     tab = line.strip().split('\t')
     content = tab[1].split(',')
-    pageRank = 0.0
-    outlinkList = []
+    c_pageRank = 0.0
 
     if len(content) > 1:
-        outlinkList = content[2:] # Input = nodeId, content
+        c_pageRank += float(content[0])
+        p_pageRank = content[0]    # the previous pageRank
+        outlink_list = content[2:] # the list of neighbors
     else:
-        pageRank += float(content[0]) # Input = outlink, pr of outlink
+        c_pageRank += float(content[0]) # We sum the contributions to pageRank
 
-    #sys.stdout.write(line)
-    sys.stdout.write('%s\t%s,%s\n' % (tab[0], pageRank, outlinkList))
+
+    sys.stdout.write('%s\t%s,%s\n' % (tab[0], c_pageRank, p_pageRank, outlink_list))
+
+
+
+
+
+
+
+
+    
--- data/process_map.py
+++ data/process_map.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python
 
 import sys
+import math
 
-#
-# This program simply represents the identity function.
-#
+'''
+This mapper is used to determine when the MapReduce algorithm 
+terminates. 
+We consider our algorithm to have converged when the change in
+pageRank for all nodes between iterations is less than the
+variable t.
+'''
+
+
+# Input:
+# Each input line is formatted as NodeId: n_id\tc_rank, p_rank, adj_list
+# Where:
+#           n_id is the Node id
+#           \t is a tab character
+#           c_rank is the current pagerank of the node
+#           p_rank is the previous pagerank of the node
+#			adj_list is the node's neighbors, comma delineated
+#    
+# Output:
+# Each output line is formatted as 1\tconverged, NodeId: n_id\tc_rank, p_rank, adj_list
+# Where:
+#			\t is a tab character
+#			converged is 0 if |c_rank-p_rank| > t and 1 otherwise 
+#			n_id is the Node id
+#           c_rank is the current pagerank of the node
+#           p_rank is the previous pagerank of the node
+# 			adj_list is the node's neighbors, comma delineated
 
 for line in sys.stdin:
-    sys.stdout.write(line)
+	t = 0.0001
+	prevContent = line
+    tab = line.strip().split('\t')
+    content = tab[1].split(',')
+
+    c_pageRank = float(content[0])
+    p_pageRank = float(content[1])
+    converged = 1
+
+    # Return 0 as the value for converged if the change in pageRank is 
+    # larger than t
+    if (math.abs(c_pageRank-p_pageRank) > t):
+    	converged = 0
+
+    sys.stdout.write('1\t%s,%s\n' % (tab[0], converges, prevContent))
+
 
